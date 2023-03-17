080b5f39c96addaf6f0f507a16508f25fa632dda
Katja Sabando <katja.sabando@gmail.com>
2023-03-17 14:48:44 +0100

    Generate description with openai api

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,4 +1,4 @@
-import { Configuration, OpenAIApi } from "openai";
+import { Configuration, OpenAIApi } from 'openai';
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -9,30 +9,36 @@ export default async function (req, res) {
   if (!configuration.apiKey) {
     res.status(500).json({
       error: {
-        message: "OpenAI API key not configured, please follow instructions in README.md",
-      }
+        message:
+          'OpenAI API key not configured, please follow instructions in README.md',
+      },
     });
     return;
   }
 
-  const animal = req.body.animal || '';
-  if (animal.trim().length === 0) {
+  const event = req.body.event || '';
+  if (event.trim().length === 0) {
     res.status(400).json({
       error: {
-        message: "Please enter a valid animal",
-      }
+        message: 'Please enter valid event keywords',
+      },
     });
     return;
   }
 
+  //  sends API request to Openai API
   try {
     const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: generatePrompt(animal),
-      temperature: 0.6,
+      model: 'text-davinci-003',
+      prompt: generatePrompt(event),
+      temperature: 0,
+      max_tokens: 100,
+      top_p: 1,
+      frequency_penalty: 0.2,
+      presence_penalty: 0,
     });
     res.status(200).json({ result: completion.data.choices[0].text });
-  } catch(error) {
+  } catch (error) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
       console.error(error.response.status, error.response.data);
@@ -42,21 +48,21 @@ export default async function (req, res) {
       res.status(500).json({
         error: {
           message: 'An error occurred during your request.',
-        }
+        },
       });
     }
   }
 }
 
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
+// Function that generates the prompt
+function generatePrompt(event) {
+  const capitalizedEvent = event.toUpperCase();
+  return `Suggest an event description using keywords:
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+Event: filipino party, bring food and good mood!
+Description: Come join us for a fun-filled Filipino party! Bring your favorite Filipino dish to share with everyone and come ready to have a good time. We'll have music, games, and plenty of food to go around. So come with an empty stomach and a good mood, and let's celebrate Filipino culture together!
+Event: sports party, bring shoes, free entrance
+Description: Come join us for a fun sports party! We'll have a variety of sports activities to choose from, so make sure to bring your favorite pair of shoes. The best part? Entrance is free! So come on down and join us for a great time.
+Event: ${capitalizedEvent}
+Description:`;
 }
--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,33 @@
-import Head from "next/head";
-import { useState } from "react";
-import styles from "./index.module.css";
+import Head from 'next/head';
+import { useState } from 'react';
+import styles from './index.module.css';
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
+  const [eventInput, setEventInput] = useState('');
   const [result, setResult] = useState();
 
   async function onSubmit(event) {
     event.preventDefault();
     try {
-      const response = await fetch("/api/generate", {
-        method: "POST",
+      const response = await fetch('/api/generate', {
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ animal: animalInput }),
+        body: JSON.stringify({ event: eventInput }),
       });
 
       const data = await response.json();
       if (response.status !== 200) {
-        throw data.error || new Error(`Request failed with status ${response.status}`);
+        throw (
+          data.error ||
+          new Error(`Request failed with status ${response.status}`)
+        );
       }
 
       setResult(data.result);
-      setAnimalInput("");
-    } catch(error) {
+      setEventInput('');
+    } catch (error) {
       // Consider implementing your own error handling logic here
       console.error(error);
       alert(error.message);
@@ -39,17 +42,16 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        <h3>Event description</h3>
         <form onSubmit={onSubmit}>
           <input
             type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
+            name="event"
+            placeholder="Enter an event"
+            value={eventInput}
+            onChange={(e) => setEventInput(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
+          <input type="submit" value="Generate Description" />
         </form>
         <div className={styles.result}>{result}</div>
       </main>
